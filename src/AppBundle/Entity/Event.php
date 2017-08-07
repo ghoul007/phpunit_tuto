836d379c8504ad4d3362e374ac09f43bfef98533
Ahmed <ghoul.ahmed5@gmail.com>
2017-08-07 23:27:31 +0100

    add class Event

---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table(name="event")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location", type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=5, scale=2)
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Event
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set location
+     *
+     * @param string $location
+     *
+     * @return Event
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Get location
+     *
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Event
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
--- tests/AppBundle/Controller/EventControllerTest.php
+++ tests/AppBundle/Controller/EventControllerTest.php
@@ -12,6 +12,29 @@ class EventControllerTest extends WebTestCase
     public function testindex_should_list_all_events()
     {
         $client = static::createClient();
+
+        $container = $client->getContainer();
+//$container = static ::$karnel->getContainer();
+        $event = new Event();
+        $event->setName('Test MVC');
+        $event->setLocation('tunisie');
+        $event->setPrice(22);
+
+        $event1 = new Event();
+        $event1->setName('Test Cloud');
+        $event1->setLocation('france');
+        $event1->setPrice(25);
+
+//        $em = $this->getDoctrine()->getManager();
+
+        $em = $container->get('doctrine')->getManager();
+        $em->persist($event);
+        $em->persist($event1);
+        $em->flush();
+
+
+
+
         $crawler = $client->request('GET', '/events');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('event1', $client->getResponse()->getContent());
